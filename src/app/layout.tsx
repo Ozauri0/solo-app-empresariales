091351f5c708d962af36637a7aeff7e481de6e14
Christian <christianagr.62@gmail.com>
2025-04-22 18:16:29 -0400

    Se añadió la clase light y el estilo de esquema de color al elemento HTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" className="light" style={{ colorScheme: "light" }}>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
